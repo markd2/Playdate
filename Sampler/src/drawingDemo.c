46681bdf3e0b5ced96339c7095c61f6077069255
Mark Dalrymple <markd@borkware.com>
2021-06-26 16:10:20 -0400

    Moves an ellipse around with the D-pad

--- Sampler/src/drawingDemo.c
+++ Sampler/src/drawingDemo.c
@@ -9,12 +9,78 @@
 
 #include "pd_api.h"
 
+typedef struct Point {
+    int x;
+    int y;
+} Point;
+
+typedef struct Rect {
+    int x;
+    int y;
+    int width;
+    int height;
+} Rect;
+
 typedef struct DrawingDemo {
     DemoSample isa;
     ButtonPumper *pumper;
     int count;
+
+    Rect ellipseRect;
     
 } DrawingDemo;
+
+
+static void drawShapes(DrawingDemo *demo) {
+    pd->graphics->clear(kColorWhite);
+    
+    int lineWidth = 2;
+    float startAngle = 0.0;
+    float endAngle = 0.0;
+    LCDColor color = kColorBlack;
+    pd->graphics->drawEllipse(demo->ellipseRect.x, demo->ellipseRect.y,
+                              demo->ellipseRect.width, demo->ellipseRect.height,
+                              lineWidth, startAngle, endAngle, color);
+} // drawShapes
+
+
+Rect clampToScreen(Rect rect) {
+    if (rect.x < 0) rect.x = 0;
+    if (rect.y < 0) rect.y = 0;
+
+    if (rect.x + rect.width > kScreenWidth) {
+        rect.x = kScreenWidth - rect.width;
+    }
+
+    if (rect.y + rect.height > kScreenHeight) {
+        rect.y = kScreenHeight - rect.height;
+    }
+
+    return rect;
+
+} // clampToScreen
+
+
+static void moveShapes(DrawingDemo *demo) {
+    if (buttonPumperButtonIsDown(demo->pumper, kButtonLeft)) {
+        demo->ellipseRect.x--;
+    }
+
+    if (buttonPumperButtonIsDown(demo->pumper, kButtonRight)) {
+        demo->ellipseRect.x++;
+    }
+
+    if (buttonPumperButtonIsDown(demo->pumper, kButtonUp)) {
+        demo->ellipseRect.y--;
+    }
+
+    if (buttonPumperButtonIsDown(demo->pumper, kButtonDown)) {
+        demo->ellipseRect.y++;
+    }
+
+    demo->ellipseRect = clampToScreen(demo->ellipseRect);
+
+} // moveShapes
 
 
 static int update(void *context)  {
@@ -25,6 +91,9 @@ static int update(void *context)  {
     PDButtons pushed, released;
     pd->system->getButtonState(NULL, &pushed, &released);
     buttonPumperPump(demo->pumper, pushed, released);
+
+    moveShapes(demo);
+    drawShapes(demo);
 
     return 1;
 
@@ -42,6 +111,8 @@ DemoSample *drawingDemoSample(void) {
                                                      sizeof(DrawingDemo));
     demo->pumper = buttonPumperNew(handleButtons);
     demo->count = 0;
+
+    demo->ellipseRect = (Rect){ 0, 0, 50, 30 };
 
     return (DemoSample *)demo;
 } // drawingDemoSample
