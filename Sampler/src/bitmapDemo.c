ea6ead897d90e17b60b95fd45e1375fb16c2643c
Mark Dalrymple <markd@borkware.com>
2021-06-27 21:40:31 -0400

    Draws harlequin board.

--- Sampler/src/bitmapDemo.c
+++ Sampler/src/bitmapDemo.c
@@ -4,6 +4,7 @@
 #include "demoSample.h"
 #include "buttonpumper.h"
 #include "globals.h"
+#include "geometry.h"
 
 #include "pd_api.h"
 
@@ -11,6 +12,36 @@ typedef struct BitmapDemo {
     DemoSample isa;
     ButtonPumper *pumper;
 } BitmapDemo;
+
+
+const Rect kTopLeftQuadrant = (Rect){
+    0, 0,
+    kScreenWidth / 2.0, kScreenHeight / 2.0
+};
+const Rect kTopRightQuadrant = (Rect){
+    kScreenWidth / 2.0, 0,
+    kScreenWidth / 2.0, kScreenHeight / 2.0
+};
+const Rect kBottomLeftQuadrant = (Rect){
+    0, kScreenHeight / 2.0,
+    kScreenWidth / 2.0, kScreenHeight / 2.0
+};
+const Rect kBottomRightQuadrant = (Rect){
+    kScreenWidth / 2.0, kScreenHeight / 2.0,
+    kScreenWidth / 2.0, kScreenHeight / 2.0
+};
+
+
+static void fillRect(Rect rect, LCDColor color) {
+    pd->graphics->fillRect(rect.x, rect.y, rect.width, rect.height, color);
+} // fillRect
+
+static void drawShapes(void) {
+    fillRect(kTopLeftQuadrant, kColorBlack);
+    fillRect(kTopRightQuadrant, kColorWhite);
+    fillRect(kBottomLeftQuadrant, kColorWhite);
+    fillRect(kBottomRightQuadrant, kColorBlack);
+} // drawShapes
 
 
 static int update(void *context)  {
@@ -24,6 +55,8 @@ static int update(void *context)  {
 
     const char *snorgle = "snorgle bitmap";
     pd->graphics->drawText(snorgle, strlen(snorgle), kASCIIEncoding, 30, kScreenHeight / 2);
+
+    drawShapes();
 
     return 1;
 } // update
