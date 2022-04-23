0b6b1d791c4b3fa6c324fb119601551f6fa03326
MarkD <markd@borkware.com>
2022-04-23 17:09:18 -0400

    Clobbers first int of allocated blocks. Simulator often has the first 8 bytes zeroed, which can mask problems.

--- common-source/memory.c
+++ common-source/memory.c
@@ -1,5 +1,6 @@
 #include "memory.h"
 
+#include <stdint.h>
 #include "pd_api.h"
 
 #include "globals.h"
@@ -9,7 +10,15 @@
 // If size is zero, frees the given pointer.
 
 void *pdMalloc(size_t size) {
-    return pd->system->realloc(NULL, size);
+    void *blah = pd->system->realloc(NULL, size);
+
+    // Clobber the first couple of bytes - in the simulator
+    // pretty often get the first 8 bytes zeroed.
+    if (size >= 4) {
+        *((int32_t *)blah) = 0xABAD0B0E;
+    }
+
+    return blah;
 } // pdMalloc
 
 
