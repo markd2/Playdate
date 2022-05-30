6052d8b8e661057c98281c942af3ef39adc2a235
Mark Dalrymple <markd@borkware.com>
2022-05-30 07:17:21 -0400

    Fleshing out data structure

--- Trek/Trek/src/trek.h
+++ Trek/Trek/src/trek.h
@@ -20,15 +20,45 @@ bool coordinateEqual(Coordinate thing1, Coordinate thing2);
 
 enum {
     kGalaxyRows = 9,
-    kGalaxyColumns = 9
+    kGalaxyColumns = 9,
+
+    kCourseNotSet = -1
 };
+
+typedef enum Condition {
+    kConditionGreen,
+    kConditionYellow,
+    kConditionRed
+} Condition;
+
+typedef enum SectorObject {
+    kEnterprise = 'E',
+    kKlingon = 'K',
+    kStar = '*',
+    kBase = 'B',
+    kTorp = '#'
+} SectorObject;
 
 typedef struct Galaxy {
     Coordinate enterpriseSector;
     Coordinate enterpriseCoordinate;
 
+    int timeLeft; // 300 == 3 years
+    int stardate;  // e.g. 34240
+    int shields; // 0..100 (%)
+    Condition condition;
+    int shieldPercentage; // 0..100 (%)
+    int shieldEnergy;
+    int availableEnergy;
+    int torps;
+    int klingons;
+    int bases;
+    Coordinate course; // kCourseNotSet / -1,-1 for unset course
+
     Sector sectors[kGalaxyRows][kGalaxyColumns];
     bool visible[kGalaxyRows][kGalaxyColumns];
+
+    
 } Galaxy;
 
 void galaxyMakeVisibleAroundSector(Galaxy *galaxy, Coordinate sector);
