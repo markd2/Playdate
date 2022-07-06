e35274e99d57f7dcc5a66dd7ce28b1cf9042a01f
markd <markd@borkware.com>
2022-07-06 09:57:02 -0400

    New directory contents from template

---
+++ c++/README.md
@@ -0,0 +1,10 @@
+# TEMPLATE
+
+I am a template. there are many like me.
+
+Places to edit:
+
+* Makefile
+* README
+* Source/pdxinfo
+
---
+++ c++/src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "globals.h"
+
+#include "pd_api.h"
+
+// Return 1 to update the display, 0 to not update it
+static int update(void *userdata) {
+    print("SNORGLE");
+    return 1;
+} // update
+
+int eventHandler(PlaydateAPI* playdate, 
+                 PDSystemEvent event,
+                 uint32_t arg) {
+    // NOTHING GOES HERE
+    pd = playdate;
+    pd->system->logToConsole("event received %s (%x)", eventNames[event], arg);
+
+    switch (event) {
+    case kEventInit: {
+        pd->display->setRefreshRate(2);
+        LCDFont *font = pd->graphics->loadFont("/System/Fonts/Asheville-Sans-14-Bold.pft", NULL);
+        pd->graphics->setFont(font);
+
+        pd->system->setUpdateCallback(update, NULL);
+
+        break;
+    }
+
+    case kEventPause:
+        break;
+
+    case kEventResume:
+        break;
+
+    case kEventTerminate:
+        break;
+
+    default: break;
+    }
+    
+    return 0;
+
+} // eventHandler
