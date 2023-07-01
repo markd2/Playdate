352511be61f4c144abbe1c5bec06607b9bde69c5
MarkD <markd@borkware.com>
2023-06-30 22:23:20 -0400

    prettifying

--- zigdate/Zampler/src/main.zig
+++ zigdate/Zampler/src/main.zig
@@ -3,33 +3,45 @@ const pdapi = @import("playdate_api_definitions.zig");
 
 var g_playdate_image: *pdapi.LCDBitmap = undefined;
 
-pub export fn eventHandler(playdate: *pdapi.PlaydateAPI, event: pdapi.PDSystemEvent, arg: u32) callconv(.C) c_int {
-    //TODO: replace with your own code!
-
+pub export fn eventHandler(pd: *pdapi.PlaydateAPI,
+                           event: pdapi.PDSystemEvent,
+                           arg: u32) callconv(.C) c_int {
     _ = arg;
+
     switch (event) {
         .EventInit => {
-            g_playdate_image = playdate.graphics.loadBitmap("playdate_image", null).?;
-            const font = playdate.graphics.loadFont("/System/Fonts/Asheville-Sans-14-Bold.pft", null).?;
-            playdate.graphics.setFont(font);
+            g_playdate_image = pd.graphics.loadBitmap("pd_image",
+                                                      null).?;
+            const font = pd.graphics.loadFont("/System/Fonts/Asheville-Sans-14-Bold.pft", null).?;
+            pd.graphics.setFont(font);
 
-            playdate.system.setUpdateCallback(update_and_render, playdate);
+            pd.system.setUpdateCallback(updateAndRender, pd);
+        },
+        .EventPause => {
+            // setup menu
+        },
+        .EventResume => {
+            // back from menu
         },
         else => {},
     }
     return 0;
 }
 
-fn update_and_render(userdata: ?*anyopaque) callconv(.C) c_int {
+fn updateAndRender(userdata: ?*anyopaque) callconv(.C) c_int {
     //TODO: replace with your own code!
 
-    const playdate: *pdapi.PlaydateAPI = @ptrCast(@alignCast(userdata.?));
+    const pd: *pdapi.PlaydateAPI = @ptrCast(@alignCast(userdata.?));
     const to_draw = "Hello from Zig!";
 
-    playdate.graphics.clear(@intFromEnum(pdapi.LCDSolidColor.ColorWhite));
-    const pixel_width = playdate.graphics.drawText(to_draw, to_draw.len, .UTF8Encoding, 0, 0);
-    _ = pixel_width;
-    playdate.graphics.drawBitmap(g_playdate_image, pdapi.LCD_COLUMNS / 2 - 16, pdapi.LCD_ROWS / 2 - 16, .BitmapUnflipped);
+    pd.graphics.clear(@intFromEnum(pdapi.LCDSolidColor.ColorWhite));
+
+    _ = pd.graphics.drawText(to_draw, to_draw.len, .UTF8Encoding, 0, 0);
+
+    pd.graphics.drawBitmap(g_playdate_image, 
+                           pdapi.LCD_COLUMNS / 2 - 16, 
+                           pdapi.LCD_ROWS / 2 - 16,
+                           .BitmapUnflipped);
 
     //returning 1 signals to the OS to draw the frame.
     //we always want this frame drawn
