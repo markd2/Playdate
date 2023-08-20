10ea643fd2517ce53b6b4488ba945d49c7679167
Mark Dalrymple <markd@borkware.com>
2023-08-20 09:04:03 -0700

    Adds point / drawing stuff

--- zigdate/Zampler/src/animator.zig
+++ zigdate/Zampler/src/animator.zig
@@ -7,6 +7,9 @@ const util = @import("util.zig");
 
 var pd: *pdapi.PlaydateAPI = undefined;
 
+// todo - unify these constants
+pub const kButtonRight = pdapi.BUTTON_RIGHT;
+
 var g_robot_image: *pdapi.LCDBitmap = undefined;
 var g_player_image: *pdapi.LCDBitmap = undefined;
 
@@ -18,6 +21,31 @@ pub const card = cardmod.Card{
     .tick = tick
 };
 
+const Point = struct {
+    x: i16,
+    y: i16
+};
+
+const Anim = struct {
+    type: u8,
+
+    start: Point,
+    end: Point,
+
+    fn location(self: Anim, percentage: u8) Point {
+        _ = percentage;
+        return Point{ .x = self.start.x, .y = self.start.y };
+    }
+};
+
+const anim1 = Anim{ .type = 0,
+                   .start = Point{ .x = 10, .y = 10 },
+                   .end = Point{ .x = 200, .y = 10 } };
+
+const anim2 = Anim{ .type = 1,
+                   .start = Point{ .x = 10, .y = 20 },
+                   .end = Point{ .x = 150, .y = 150 } };
+const anims: [2]Anim = .{ anim1, anim2 };
 
 pub fn init(p: *pdapi.PlaydateAPI) void {
     pd = p;
@@ -26,22 +54,41 @@ pub fn init(p: *pdapi.PlaydateAPI) void {
     g_player_image = pd.graphics.loadBitmap("player", null).?;
 }
 
+
 pub fn draw() void {
     const white = @intFromEnum(pdapi.LCDSolidColor.ColorWhite);
     pd.graphics.clear(white);
-
-    util.drawBitmap(g_player_image, 10, 10);
-    util.drawBitmap(g_robot_image, 60, 80);
 
     const text = "(A) to animate.   (>) to Step";
 
     const x = 5;
     const y = 220;
     _ = pd.graphics.drawText(text.ptr, text.len, .ASCIIEncoding, x, y);
+
+
+    for (anims) | anim | {
+        const p = anim.location(percent);
+        switch (anim.type) {
+            0 => util.drawBitmap(g_player_image, p.x, p.y),
+            1 => util.drawBitmap(g_robot_image, p.x, p.y),
+            else => {}
+        }
+    }
 }
+
+var percent: u8 = 0;
 
 pub fn tick (deltaTime: u32) bool {
     _ = deltaTime;
+
+    var current: pdapi.PDButtons = undefined;
+    pd.system.getButtonState(&current, null, null);
+
+    if (current & kButtonRight != 0) {
+        util.mongoLog("right ", .{});
+        percent += 10;
+        if (percent > 100) percent = 0;
+    }
 
     draw();
     return true;
