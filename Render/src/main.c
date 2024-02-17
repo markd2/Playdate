ad1dfa9d7ebd6383e4bb55c15625212e96ff8cba
MarkD <markd@borkware.com>
2024-02-17 18:07:18 -0500

    Lots o' "sprites"

--- Render/src/main.c
+++ Render/src/main.c
@@ -5,13 +5,22 @@
 
 PlaydateAPI *pd;
 
-static const int screenWidth = 400;
-static const int screenHeight = 240;
-
 typedef enum Color {
     Color_Dark,
     Color_Light
 } Color;
+
+static const int screenWidth = 400;
+static const int screenHeight = 240;
+
+// use #define so we can statically "allocate" these buffer
+#define screenBufferSize 12480 // screenHeight * LCD_ROWSIZE
+
+//static const int screenBufferSize = 12480; // screenHeight * LCD_ROWSIZE;
+
+static const uint8_t darkScreen[screenBufferSize] = { [0 ... screenBufferSize - 1] = Color_Dark };
+static const uint8_t lightScreen[screenBufferSize] = { [0 ... screenBufferSize - 1] = Color_Light };
+static const uint8_t stripeScreen[screenBufferSize] = { [0 ... screenBufferSize - 1] = 0xCC };
 
 typedef struct Rect {
     int x, y;
@@ -24,24 +33,49 @@ static void moveAndDrawRect(Rect from, Rect to);
 static void plotPoint(uint8_t *buffer, int x, int y, Color color);
 static void drawLine(uint8_t *buffer);
 static void fillRect(uint8_t *buffer, Rect rect, Color color);
-static void moveSprite(uint8_t *buffer);
+static void moveSprite(uint8_t *buffer, Rect *inoutSprite);
+static void moveSprites(uint8_t *buffer);
+static void bulkBlortFromTo(const uint8_t *from, uint8_t *to);
 
+static const int spriteWidth = 10;
+static const int spriteHeight = 7;
 
-static Rect sprite = { 0, 0, 20, 30 };
+static Rect sprite = { 0, 0, spriteWidth, spriteHeight };
+static Rect sprites[150];
 
 static int frameCounter = 0;
+
+static void firstTimeSetup(void) {
+    Rect *scan, *stop;
+    scan = sprites;
+    stop = scan + sizeof(sprites) / sizeof(*sprites);
+
+    int hmod = screenWidth - spriteWidth;
+    int vmod = screenHeight - spriteHeight;
+
+    while (scan < stop) {
+        scan->x = rand() % hmod;
+        scan->y = rand() % vmod;
+        scan->width = 8 + rand() % spriteWidth;
+        scan->height = 6 + rand() % spriteHeight;
+        scan++;
+    }
+} // firstTimeSetup
 
 static int update(void* userdata) {
     pd = userdata;
 
     uint8_t *frameBuffer = pd->graphics->getFrame();
+
+//    bulkBlortFromTo(stripeScreen, frameBuffer);
+//    bulkBlortFromTo(darkScreen, frameBuffer);
+//    bulkBlortFromTo(lightScreen, frameBuffer);
 	
-    // pd->graphics->clear(kColorWhite);
-    pd->display->setRefreshRate(0);
+//    pd->graphics->clear(kColorWhite);
         
 //    drawStaticBackground();
 
-    moveSprite(frameBuffer);
+    moveSprites(frameBuffer);
 
     pd->system->drawFPS(0, 0);
 
@@ -51,9 +85,32 @@ static int update(void* userdata) {
 } // update
 
 
-static void moveSprite(uint8_t *buffer) {
+// This slows everything down to 50fps on the sim, 52fps on the device
+static void bulkBlortFromTo(const uint8_t *from, uint8_t *to) {
+    memcpy(to, from, screenBufferSize);
+    // looked at https://cboard.cprogramming.com/c-programming/154333-fast-memcpy-alternative-32-bit-embedded-processor-posted-just-fyi-fwiw.html, but it still was 52 fps on the device.x
+    pd->graphics->markUpdatedRows(0, screenHeight);
 
-    Rect oldRect = sprite;
+} // bulkBlort
+
+
+static void moveSprites(uint8_t *buffer) {
+    Rect *scan, *stop;
+    scan = sprites;
+    stop = scan + sizeof(sprites) / sizeof(*sprites);
+
+    while (scan < stop) {
+        moveSprite(buffer, scan);
+        scan++;
+    }
+
+} // moveSprites
+
+
+static void moveSprite(uint8_t *buffer, Rect *inoutSprite) {
+
+    Rect oldRect = *inoutSprite;
+    Rect sprite = *inoutSprite;
     sprite.x += 2;
     sprite.y += 1;
     if (sprite.x + sprite.width >= screenWidth) {
@@ -65,6 +122,8 @@ static void moveSprite(uint8_t *buffer) {
 
     fillRect(buffer, oldRect, Color_Light);
     fillRect(buffer, sprite, Color_Dark);
+
+    *inoutSprite = sprite;
 
 } // moveSprite
 
@@ -151,6 +210,9 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg) {
         // handler, the system assumes the game is pure C and doesn't
         // run any Lua code in the game
         pd->system->setUpdateCallback(update, pd);
+        pd->display->setRefreshRate(0);
+
+        firstTimeSetup();
     }
 	
     return 0;
