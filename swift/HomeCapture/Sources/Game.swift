8372dae8c4c62249054953d7e03334fb50d73ae0
MarkD <markd@borkware.com>
2024-03-18 16:34:57 -0400

    Draw some (static) text

--- swift/HomeCapture/Sources/Game.swift
+++ swift/HomeCapture/Sources/Game.swift
@@ -1,6 +1,9 @@
 import Playdate
 
 struct Game {
+    var x: Int32 = 0
+    var y: Int32 = 0
+    
 
     init() {
         // Setup the device before any other operations.
@@ -10,6 +13,16 @@ struct Game {
     
     mutating func updateGame() {
         sys.drawFPS(0, 0)
+
+        let text: StaticString = "splunge"
+        text.withUTF8Buffer { utf8 in
+            _ = graphics.drawText(utf8.baseAddress, utf8.count, PDStringEncoding.kUTF8Encoding, x, y)
+        }
+
+        x += 10
+        y += 10
+        if x > 400 { x = rand() % 120 }
+        if y > 250 { y = rand() % 50 }
     }
 }
 
