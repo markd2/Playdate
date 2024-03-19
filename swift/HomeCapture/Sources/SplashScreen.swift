1206e26dc52b1515cfa6da3739ec7fd7dd9db5cf
MarkD <markd@borkware.com>
2024-03-18 20:52:40 -0400

    Print 2D Home Capture

--- swift/HomeCapture/Sources/SplashScreen.swift
+++ swift/HomeCapture/Sources/SplashScreen.swift
@@ -4,25 +4,35 @@ class SplashScreen: GameMode {
     var x: Int32 = 0
     var y: Int32 = 0
 
-    var text: StaticString  = "splunge"
+    var text: StaticString  = "2D Home Capture"
 
     override func reset() {
         x = 0
         y = 0
-        text = "splunge"
+        text = "2D Home Capture"
     }
 
+    var frameSkip = 20
+
     override func updateGame() -> State {
-        pd.sys.drawFPS(0, 0)
-        
-        text.withUTF8Buffer { utf8 in
-            _ = pd.graphics.drawText(utf8.baseAddress, utf8.count, PDStringEncoding.kUTF8Encoding, x, y)
+        defer {
+            frameSkip -= 1
+            if frameSkip < 0 { frameSkip = 10 }
         }
+
+        pd.sys.drawFPS(0, 0)
+
         
-        x += 10
-        y += 10
-        if x > 400 { x = rand() % 120 }
-        if y > 250 { y = rand() % 50 }
+        if frameSkip == 0 {
+            text.withUTF8Buffer { utf8 in
+                _ = pd.graphics.drawText(utf8.baseAddress, utf8.count, PDStringEncoding.kUTF8Encoding, x, y)
+            }
+        
+            x += 10
+            y += 20
+            if x > 400 { x = rand() % 120 }
+            if y > 250 { y = rand() % 50 }
+        }
         
         var pushed = PDButtons(rawValue: 0)
         pd.sys.getButtonState(nil, &pushed, nil)
@@ -36,7 +46,7 @@ class SplashScreen: GameMode {
             text = "B BUTTON"
         } else {
             // pd.graphics.clear(1)
-            text = "Splunge"
+            text = "2D Home Capture"
         }
 
         return .loco
